603ac7af3d6a59501a1b7a3eac5f703486ea2ca9
장성민 <earnestgem@optatumplatform.com>
2022-04-21 17:27:36 +0900

    BettingComponent.vue 에 template 코드 작성하기

--- src/components/BettingComponent.vue
+++ src/components/BettingComponent.vue
@@ -1,5 +1,21 @@
 <template>
+  <div class="betting">
+    <h1>Numbergame BApp Test</h1>
+    Amount: <input v-model="amount" placeholder="0 Klay"/>
 
+    <ul>
+      <li v-on:click="clickNumber">1</li>
+      <li v-on:click="clickNumber">2</li>
+      <li v-on:click="clickNumber">3</li>
+      <li v-on:click="clickNumber">4</li>
+      <li v-on:click="clickNumber">5</li>
+    </ul>
+    <div v-if="pending" id="loader">Loading...</div>
+    <div class="event" v-if="winEvent">
+      Won: {{winEvent.result}}
+      Reward: {{winEvent.amount}} Klay
+    </div>
+  </div>
 </template>
 
 <script>
@@ -47,5 +63,35 @@ export default {
 </script>
 
 <style scoped>
-
+.betting {
+  margin-top: 50px;
+  text-align: center;
+}
+ul {
+  margin: 25px;
+  list-style-type: none;
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  grid-column-gap: 25px;
+}
+li {
+  padding: 20px;
+  margin-right: 3px;
+  border-radius: 30%;
+  cursor: pointer;
+  background-color: #fff;
+  color: #4b08e0;
+  box-shadow: 3px 5px 1px #4b08e0;
+}
+li:hover {
+  background-color: #4b08e0;
+  color: white;
+  box-shadow: 0px 0px 1px #4b08e0;
+}
+li:active {
+  opacity: 0.7;
+}
+* {
+  color: #444444;
+}
 </style>
